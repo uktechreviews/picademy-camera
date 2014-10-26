45361b0889e84a2555e13ef8f48843461f8f5cf5
spencerorgan <spencerorgan1@me.com>
2014-10-26 21:52:39 +0000

    Add scary

--- mycamera.py
+++ mycamera.py
@@ -2,7 +2,7 @@ from picamera import PiCamera
 with PiCamera() as camera:
 	raw_input("Are you scared?")
 	camera.start_preview()
-	camera.annotate_text='Watch out for the ghosts!!!!!!'
+	camera.annotate_text='Watch out for the scary ghosts!!!!!!'
 	camera.capture('/home/pi/scare.jpg')
 camera.stop_preview()
     

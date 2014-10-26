2063e5430d3de0e3cb1e8c0971ed044c264844dd
spencerorgan <spencerorgan1@me.com>
2014-10-26 22:24:36 +0000

    fix for Python 3, close #2

--- mycamera.py
+++ mycamera.py
@@ -1,6 +1,6 @@
 from picamera import PiCamera
 with PiCamera() as camera:
-	raw_input("Are you scared?")
+	input("Are you scared?")
 	camera.start_preview()
 	camera.annotate_text='Watch out for the scary ghosts!!!!!!'
 	camera.capture('/home/pi/scare.jpg')

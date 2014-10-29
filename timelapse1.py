65fad99ee7d566c29fedf6a0d9eeb60ef2c4b9fb
spencerorgan <spencerorgan1@me.com>
2014-10-29 08:24:54 +0000

    edit time scale

--- timelapse1.py
+++ timelapse1.py
@@ -1,17 +1,20 @@
-# Timelapse camera taking a photo every 30 minutes for 10 hours
+# Timelapse camera taking a photo every 10 minutes for 12 hours
 from picamera import PiCamera
 import time
-frames = 20
-timebetween = 1800
+frames = 72
+timebetween = 600
 actual_timebetween = timebetween - 6
 framecount = 0
 
+
 while framecount < frames: 
 	with PiCamera() as camera:
+#		camera.start_preview()
 		localtime = time.asctime( time.localtime(time.time()) )
 		camera.annotate_text=localtime
 		camera.capture('/home/pi/photo_output/image%s.jpg'%(framecount))
 		framecount +=1
+#		camera.stop_preview()
 		time.sleep(actual_timebetween)
 		  
 
